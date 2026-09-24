97191ae05fc55cd634c607c6bafd299568205ee8
Ana Beatrize <ana.silva154@alunos.sc.senac.br>
2026-09-24 14:11:10 -0300

    barradepesquisaquasela

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { View, Text, StyleSheet, FlatList, Image } from "react-native";
 import DadosDosFilmes from '../componentes/DadosDosFilmes';
 import renderCategoria from '../componentes/CardCategorias';
+import InputBusca from '../componentes/inputBusca';
 
 const categorias = DadosDosFilmes();
 console.log(categorias);
@@ -8,6 +9,7 @@ console.log(categorias);
 export default function App() {
   return (
     <View style={styles.container}>
+      <InputBusca></InputBusca>
       <FlatList
         data={categorias}
         keyExtractor={(item) => item.id}
